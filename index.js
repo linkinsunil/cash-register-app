35a54aec0b89528e0d17e911094bbb340bb381c3
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-15 17:32:52 +0530

    update:all html elements binded in js

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-let billAmount = document.querySelector(".bill-amount");
-let cashTaken = document.querySelector(".cash-taken")
-let toReturn = document.querySelector(".cash-taken")
-let denomination = document.querySelector(".denomination")
+const billAmount = document.querySelector("#bill-amount")
+const cashTaken = document.querySelector("#cash-taken")
+const button = document.querySelector("#check-btn")
+const toReturn = document.querySelector("#to-return")
+const denomination = document.querySelector("#denomination")
+
 let currency = [1,5,10,20,100,500,2000];
 
 
-function billing(billAmount, cashTaken) {
-    var billAmount = readLineSync.question("Billed Amount: ");
-    var cashTaken = readLineSync.question("Cash Taken: ");
-    var cashToReturn = cashTaken - billAmount; 
-    console.log("Cash to return: ", cashToReturn);
-    
-    var remainder = cashToReturn;
-    for (var i=currency.length-1; i>=0; i--) {
-      if (remainder >= currency[i]) {
-        var notes = remainder/currency[i];
-        var remainder = remainder % currency[i];
-        console.log("Denomination/Number   ", currency[i]," / ", Math.floor(notes));
-      }       
-    }
-}
 
-console.log(billing(210, 2000));

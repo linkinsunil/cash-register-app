2ead369701cf268fa19b2e89eb5ae26465707859
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-15 21:29:24 +0530

    update: table

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const billAmount = document.querySelector("#bill-amount")
 const cashTaken = document.querySelector("#cash-taken")
 const button = document.querySelector("#check-btn")
-const toReturn = document.querySelector("#to-return")
+const msgReturn = document.querySelector("#msg-return")
 const changeTable = document.querySelector("#change-table")
 
 let currency = [1,5,10,20,100,500,2000];
 
-
+console.log(changeTable)
 

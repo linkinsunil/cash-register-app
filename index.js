068dd8544e19c42ef0743c0da1eb1f760a7a2988
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-15 22:49:10 +0530

    wip js

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ const cashTaken = document.querySelector("#cash-taken")
 const button = document.querySelector("#check-btn")
 const msgReturn = document.querySelector("#msg-return")
 const changeTable = document.querySelector("#change-table")
+const message = document.querySelector("#message")
 
 let currency = [1,5,10,20,100,500,2000];
 
-console.log(changeTable)
+button.addEventListener("click", function validate(){
+  message.style.display = "none";
+  if(billAmount.value > 0){
+    if(cashTaken.value >= billAmount.value){
+      // const returnableAmount = cashTaken.value - billAmount.value;
+      // calculateChange(returnableAmount);
+    } else {
+      message.style.display = "block";
+      message.innerText = "Invalid Amount. Cash Taken must be greater than or equal to the Bill Amount"
+    }
+  } else {
+    message.style.display = "block";
+    message.innerText= "Bill Amount must be greater than 0"   
+  }
+
+});
 

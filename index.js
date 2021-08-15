254cb165ba7f3f79b69afe66b38c5a864321b0f8
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-15 21:12:21 +0530

    wip

--- index.js
+++ index.js
@@ -2,26 +2,9 @@ const billAmount = document.querySelector("#bill-amount")
 const cashTaken = document.querySelector("#cash-taken")
 const button = document.querySelector("#check-btn")
 const toReturn = document.querySelector("#to-return")
-const denomination = document.querySelector("#denomination")
+const changeTable = document.querySelector("#change-table")
 
 let currency = [1,5,10,20,100,500,2000];
-const bill = billAmount.value;
-const cash = cashTaken.value;
 
 
-const handleclick = (cash, bill) => {
-  toReturn.innerText = cash - bill;
-}
-
-function cashManager(){
-
-    
-    
-
-
-
-
-
-
-}
 

c8206bd00e43576d76ea1481ee0b06c7f472744e
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-19 15:13:53 +0530

    update: fixed msgReturn bug

--- index.js
+++ index.js
@@ -2,34 +2,38 @@ const billAmount = document.querySelector("#bill-amount")
 const cashTaken = document.querySelector("#cash-taken")
 const button = document.querySelector("#check-btn")
 const msgReturn = document.querySelector("#msg-return")
-// const changeTable = document.querySelector("#change-table")
+const changeTable = document.querySelector("#change-table")
 const message = document.querySelector("#message")
 const noOfNotes = document.querySelector(".no-of-notes")
 
+// console.log(billAmount, cashTaken, msgReturn )
 
 let availableNotes = [2000,500,100,20,10,5,1];
 
-button.addEventListener("click", function validate(){
-  message.style.display = "none";
+button.addEventListener("click", validate)
 
-  if(billAmount.value > 0){
+function validate(){
 
-    if(Number(cashTaken.value) >= Number(billAmount.value)){
-      const returnableAmount = cashTaken.value - billAmount.value;
-      msgReturn.innerText = returnableAmount;
-      calculateChange(returnableAmount);
+    message.style.display = "none";
+
+    if(billAmount.value > 0){
+
+      if(Number(cashTaken.value) >= Number(billAmount.value)){
+        const returnableAmount = cashTaken.value - billAmount.value;
+        msgReturn.value = returnableAmount;
+        calculateChange(returnableAmount);
+
+      } else {
+        message.style.display = "block";
+        message.innerText = "Invalid Amount. Cash Taken must be greater than or equal to the Bill Amount"
+      }
 
     } else {
       message.style.display = "block";
-      message.innerText = "Invalid Amount. Cash Taken must be greater than or equal to the Bill Amount"
+      message.innerText= "Bill Amount must be greater than 0"   
     }
 
-  } else {
-    message.style.display = "block";
-    message.innerText= "Bill Amount must be greater than 0"   
-  }
-
-});
+};
 
 function calculateChange(returnableAmount){
   for (let i = 0; i < availableNotes.length; i++) {
